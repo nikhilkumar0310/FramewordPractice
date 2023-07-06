fb25357f327d56ec36ce3a597b994d86f3a98743
nikhilkumar0310 <kumar.nikhil0310@gmail.com>
2023-07-07 01:22:16 +0530

    Changes related to Page Factory in the LoginPage Class only

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,7 +1,10 @@
 package pages;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 import baseClass.BaseClass;
 
@@ -9,20 +12,28 @@ public class LoginPage extends BaseClass{
 
 	public LoginPage(RemoteWebDriver driver) {
 		this.driver = driver;
+		PageFactory.initElements(driver, this);
 	}
 	
+	@FindBy(how = How.ID, using = "username") WebElement userName;
 	public LoginPage enterUserName() {
-		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		//driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		userName.sendKeys("Demosalesmanager");
 		return this;
 	}
 	
+	
+	@FindBy(id = "password") WebElement passWord;
 	public LoginPage enterPassword() {
-        driver.findElement(By.id("password")) .sendKeys("crmsfa");
+        //driver.findElement(By.id("password")) .sendKeys("crmsfa");
+        passWord.sendKeys("crmsfa");
         return this;
 	}
 	
+	@FindBy(how = How.XPATH, using = "//input[@class='decorativeSubmit']") WebElement loginBtn;
 	public HomePage1 clickOnTheLoginBtn() {
-		driver.findElement(By.xpath("//input[@class='decorativeSubmit']")).click();
+		//driver.findElement(By.xpath("//input[@class='decorativeSubmit']")).click();
+		loginBtn.click();
 		return new HomePage1(driver);
 		
 	}
